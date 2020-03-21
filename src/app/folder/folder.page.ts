32ed0c5103d39d6751daaa473e4afa62b3419ab6
mlabieniec <michaellabieniec@gmail.com>
2020-03-21 16:28:33 -0400

    clean up list items in dashboard and bookmarks

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,55 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { All, Country, CovidService, State } from '../services/covid.service';
-import { LoadingController } from '@ionic/angular';
-import { BookmarkService } from '../services/bookmark.service';
-import { GeoService } from '../services/geo.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { All, Country, CovidService, State } from "../services/covid.service";
+import { LoadingController } from "@ionic/angular";
+import { BookmarkService } from "../services/bookmark.service";
+import { GeoService } from "../services/geo.service";
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  selector: "app-folder",
+  templateUrl: "./folder.page.html",
+  styleUrls: ["./folder.page.scss"]
 })
 export class FolderPage implements OnInit {
   public folder: string;
   public all: All;
-  public countries: Country[] = []
+  public countries: Country[] = [];
   public states: State[] = [];
   public today = new Date();
-  public percentRecovered:string;
-  public percentDied:string;
+  public percentRecovered: string;
+  public percentDied: string;
+  public percentCountryRecovered: string;
+  public percentCountryDied: string;
+  public percentStateRecovered: string;
+  public percentStateDied: string;
   public objectKeys = Object.keys;
-  countryData:any;
-  regionData:any;
-  bookmarks:any;
+  countryData: any;
+  regionData: any;
+  bookmarks: any;
   descending: boolean = false;
   order: number;
-  column: string = 'country';
+  column: string = "country";
 
   constructor(
-    private activatedRoute: ActivatedRoute, 
-    private covid:CovidService, 
-    private bookmarkService:BookmarkService,
+    private activatedRoute: ActivatedRoute,
+    private covid: CovidService,
+    private bookmarkService: BookmarkService,
     public loadingController: LoadingController,
-    private geoService:GeoService ) { }
+    private geoService: GeoService
+  ) {}
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.folder = this.activatedRoute.snapshot.paramMap.get("id");
     switch (this.folder) {
-      case 'Dashboard':
+      case "Dashboard":
         if (!this.all) {
           this.getAllData();
         }
         break;
-      case 'Countries':
-        if(!this.countries || this.countries.length === 0)
+      case "Countries":
+        if (!this.countries || this.countries.length === 0)
           this.getCountriesData();
-        this.column = 'country';
+        this.column = "country";
         break;
-      case 'Regions':
-        if (!this.states || this.states.length === 0)
-          this.getStatesData();
-        this.column = 'state';
+      case "Regions":
+        if (!this.states || this.states.length === 0) this.getStatesData();
+        this.column = "state";
         break;
     }
   }
@@ -60,41 +64,70 @@ export class FolderPage implements OnInit {
 
   async initLocation() {
     const loading = await this.loadingController.create({
-      message: 'Getting Your Location Data...',
+      message: "Getting Your Location Data...",
       duration: 2000
     });
     loading.present();
     try {
       const geo = await this.geoService.getLocation();
       if (geo && geo.location && geo.location.country) {
-        this.countryData = await this.covid.country(geo.location.country).toPromise();
+        this.countryData = await this.covid
+          .country(geo.location.country)
+          .toPromise();
+        this.percentCountryRecovered =
+          (
+            ((this.countryData.recovered as any) /
+              (this.countryData.cases as any)) *
+            100
+          ).toFixed(2) + "%";
+        this.percentCountryDied =
+          (
+            ((this.countryData.deaths as any) /
+              (this.countryData.cases as any)) *
+            100
+          ).toFixed(2) + "%";
       } else {
-        console.log('Could not load country data');
+        console.log("Could not load country data");
       }
       if (geo && geo.location && geo.location.region) {
-          this.covid.states().toPromise().then((states:Array<State>) => {
-            states.forEach((state:State) => {
+        this.covid
+          .states()
+          .toPromise()
+          .then((states: Array<State>) => {
+            states.forEach((state: State) => {
               if (state.state === geo.location.region) {
                 this.regionData = state;
+                this.percentStateRecovered =
+                  (
+                    ((this.regionData.recovered as any) /
+                      (this.regionData.cases as any)) *
+                    100
+                  ).toFixed(2) + "%";
+                this.percentStateDied =
+                  (
+                    ((this.regionData.deaths as any) /
+                      (this.regionData.cases as any)) *
+                    100
+                  ).toFixed(2) + "%";
               }
             });
           });
       } else {
-        console.log('Could not load region data');
+        console.log("Could not load region data");
       }
     } catch (error) {
-      console.log('Failed to load location data');
+      console.log("Failed to load location data");
       console.log(error);
     }
     loading.dismiss();
   }
 
-  sort(){
+  sort() {
     this.descending = !this.descending;
     this.order = this.descending ? 1 : -1;
   }
 
-  addBookmark(item:Country|State) {
+  addBookmark(item: Country | State) {
     this.bookmarkService.add(item);
   }
 
@@ -102,22 +135,28 @@ export class FolderPage implements OnInit {
     this.bookmarkService.remove(item);
   }
 
-  async getAllData() { 
+  async getAllData() {
     const loading = await this.loadingController.create({
-      message: 'Getting World Stats...',
+      message: "Getting World Stats...",
       duration: 2000
     });
-    loading.present();   
+    loading.present();
     try {
       this.all = await this.covid.all().toPromise();
-      this.percentRecovered = ((this.all.recovered as any) / (this.all.cases as any)*100).toFixed(2) + '%';
-      this.percentDied = ((this.all.deaths as any) / (this.all.cases as any)*100).toFixed(2) + '%';
+      this.percentRecovered =
+        (((this.all.recovered as any) / (this.all.cases as any)) * 100).toFixed(
+          2
+        ) + "%";
+      this.percentDied =
+        (((this.all.deaths as any) / (this.all.cases as any)) * 100).toFixed(
+          2
+        ) + "%";
       this.bookmarks = this.bookmarkService.bookmarks;
       this.bookmarkService.bookmarkSub.subscribe(
-        (item:any) => this.bookmarks = this.bookmarkService.bookmarks
+        (item: any) => (this.bookmarks = this.bookmarkService.bookmarks)
       );
     } catch (error) {
-      console.log('Failed to load covid data', error);
+      console.log("Failed to load covid data", error);
     }
     loading.dismiss();
     setTimeout(() => this.initLocation());
@@ -125,7 +164,7 @@ export class FolderPage implements OnInit {
 
   async getCountriesData() {
     const loading = await this.loadingController.create({
-      message: 'Please wait...',
+      message: "Please wait...",
       duration: 2000
     });
     loading.present();
@@ -135,12 +174,11 @@ export class FolderPage implements OnInit {
 
   async getStatesData() {
     const loading = await this.loadingController.create({
-      message: 'Please wait...',
+      message: "Please wait...",
       duration: 2000
     });
     loading.present();
     this.states = await this.covid.states().toPromise();
     loading.dismiss();
   }
-
 }
